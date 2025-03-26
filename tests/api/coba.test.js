cfec528862540ad74e963812740296bac11b8534
DESKTOP\Gagah <m.gagahwijaya@gmail.com>
2025-03-26 15:03:43 +0700

    Test update
    test

--- tests/api/coba.test.js
+++ tests/api/coba.test.js
@@ -1,8 +1,10 @@
 import fetch from 'node-fetch';
 import { expect } from 'chai';
+import Ajv from 'ajv';
+import schema_post from '../schema/dummySchema.js';
+import schema_get from '../schema/dummySchema2.js';
 
 describe("API Test Suite coba", function(){
-    const baseURL = 'https://dummyjson.com'
 
     it("Get Test", async function(){
         //tembak url dummyjson
@@ -11,9 +13,18 @@ describe("API Test Suite coba", function(){
         //validasi http status nya harus 200
         expect(hasil.status, 'ada yang salah').to.equal(200)
 
+        //validasi json schema
+        const ajv = new Ajv()
+        const data = await hasil.json();
+        const cekcek = ajv.compile(schema_get)
+        const hasil_schema2 = cekcek(data)
+
+        expect(hasil_schema2, 'schema is not valid').to.be.true
+
     });
 
     it("Post Test", async function(){
+        //tembak url dummyjson
         const newPost = {
             "title": "Gagah" ,
             "userId": 5
@@ -24,6 +35,17 @@ describe("API Test Suite coba", function(){
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(newPost)
         })
-    })
+
+        //validasi http status nya harus 201
+        expect(hasilpost.status).to.equal(201)
+
+        //validasi json schema
+        const ajv = new Ajv()
+        const data = await hasilpost.json();
+        const cekcek = ajv.compile(schema_post)
+        const hasil_schema = cekcek(data)
+
+        expect(hasil_schema, 'schema is not valid').to.be.true
+    });
 
 })
---
+++ tests/schema/dummySchema.js
@@ -0,0 +1,22 @@
+const schema_post = {
+
+    "type": "object",
+    "properties": {
+      "id": {
+        "type": "integer"
+      },
+      "title": {
+        "type": "string"
+      },
+      "userId": {
+        "type": "integer"
+      }
+    },
+    "required": [
+      "id",
+      "title",
+      "userId"
+    ]
+  }
+  
+export default schema_post
